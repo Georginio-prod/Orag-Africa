7ac3b6fe6a7811d07e5085b5190e3617cd2ff326
Georginio-prod <etonameklou6@gmail.com>
2026-02-04 13:34:21 +0000

    feat: add new components for PourquoiChoisir, Solution, SolutionCard, SolutionProfil, StoreButtons, Team, Testimonial, and Traction sections
    - Created `PourquoiChoisir.vue` to highlight reasons to choose OrgaAfrica.
    - Developed `Solution.vue` and `SolutionCard.vue` for displaying restaurant solutions.
    - Introduced `SolutionProfil.vue` to showcase solutions tailored for different profiles.
    - Added `StoreButtons.vue` for app download links.
    - Implemented `Team.vue` to present team members with social media links.
    - Created `Testimonial.vue` for displaying customer testimonials with a sliding feature.
    - Developed `Traction.vue` to showcase traction metrics and confidence indicators.
    - Updated global styles in `style.css` to enhance the overall appearance and maintain a consistent dark theme.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from "./components/Navbar.vue";
+import HeroSection from "./components/HeroSection.vue";
+import Solution from "./components/Solution.vue";
+import Plateforme from "./components/Plateforme.vue";
+import SolutionProfil from "./components/SolutionProfil.vue";
+import PourquoiChoisir from "./components/PourquoiChoisir.vue";
+
+// import Team from './components/Team.vue'
+import Traction from "./components/Traction.vue";
+import Testimonial from "./components/Testimonial.vue";
+import Footer from "./components/Footer.vue";
+import CTASection from "./components/CTASection.vue";
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Navbar />
+  <HeroSection />
+  <Solution />
+  <Plateforme />
+  <SolutionProfil />
+  <PourquoiChoisir />
+  <Traction />
+  <!-- <Team /> -->
+  <Testimonial />
+  <CTASection />
+  <Footer />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
